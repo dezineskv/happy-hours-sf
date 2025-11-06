21e01cd17147f265b6ffdef0087a8a856c97a5d5
KimV <71965590+dezineskv@users.noreply.github.com>
2025-11-05 20:50:06 -0800

    update to theme-provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,9 +22,10 @@ export default function RootLayout({ children }) {
         <ThemeProvider
           attribute="data-theme"
           defaultTheme="synthwave"
-          // enableSystem={false}
+          enableSystem
+          disableTransitionOnChange
         >
-          {children}
+ {children}
         </ThemeProvider>
       </body>
     </html>
